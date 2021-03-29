a7a9c042f790bf82c817184798acbbde4af45a5f
eswanthdev <53303165+eswanthdev@users.noreply.github.com>
2021-03-29 03:32:37 +0100

    Update App.java

--- src/main/java/wipro/App.java
+++ src/main/java/wipro/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldssss!" );
+        System.out.println( "Hello from austienmarizo!" );
     }
 }
