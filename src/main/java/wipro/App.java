932616c51dbe724039b19dcd4495f12b0f1d9f83
eswanthdev <53303165+eswanthdev@users.noreply.github.com>
2021-03-31 04:13:52 +0100

    pipe1

--- src/main/java/wipro/App.java
+++ src/main/java/wipro/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from austienmarizdd!");
+        System.out.println( "Hello from austienmariwzdd!");
     }
 }
