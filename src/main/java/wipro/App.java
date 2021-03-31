390362bdc91b57b964db21968769e4ad8acdb80d
eswanthdev <53303165+eswanthdev@users.noreply.github.com>
2021-03-31 04:15:26 +0100

    Update App.java

--- src/main/java/wipro/App.java
+++ src/main/java/wipro/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from austienmariwzdd!");
+        System.out.println( "Hello from tjis os so s0os ps s[s austienmariwzdd!");
     }
 }
